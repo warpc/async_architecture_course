5e31f30b44a1de049502fcda808d425b684aef0f
Vladimir Sharshov <vladimir.sharshov@gett.com>
2021-11-19 17:45:02 +0300

    Add current_user helper to Billing

--- systems/billing/app/controllers/application_controller.rb
+++ systems/billing/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def manager_access?
+    current_user && current_user.manager_access?
+  end
 end
